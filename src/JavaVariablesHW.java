0823ff7e607d9a9a8c03bc397340effa0beeb3b0
angela-wheatley <79345893+angela-wheatley@users.noreply.github.com>
2021-03-07 22:26:28 -0600

    First commit of this project

---
+++ src/JavaVariablesHW.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class JavaVariablesHW
+{
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("What is your favorite food?");
+        
+        System.out.printf("Where do you like to eat %s?\n", favFood);
+        String fav-Rest = input.nextLine();
+        
+        System.out.printf("Would you like to get some %s with me at %s?\n", favFood, fav-Rest);
+        
+        if (&ANSwer.equals("yes") || &ANSwer.equals("okay") || &ANSwer.equals("sure"))
+        {
+            System.out.println("Great, see you there!");
+        }
+        else if (&ANSwer.equals("maybe"))
+        {
+            System.out.println("Ok, let me know!");
+        }
+        else
+        {
+            System.out.println("Maybe next time.");
+        }
+               
+    }
+
+}
